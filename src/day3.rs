45ef88fbbd8a771cc9212c30d34fd8d912e7f26a
Giacomo Stevanato <giaco.stevanato@gmail.com>
2021-12-03 08:57:44 +0100

    Improve day3 solution

--- src/day3.rs
+++ src/day3.rs
@@ -1,68 +1,54 @@
 #[allow(unused_imports)]
 use super::prelude::*;
-type Input = Vec<Vec<bool>>;
+type Input = Vec<u16>;
+
+const NUM_LEN: usize = 12;
 
 pub fn input_generator(input: &str) -> Input {
     input
         .lines()
-        .map(|line| line.chars().map(|c| c == '1').collect_vec())
+        .map(|line| {
+            line.chars()
+                .fold(0, |acc, c| (acc << 1) + (c == '1') as u16)
+        })
         .collect_vec()
 }
 
+fn get_bit(n: u16, pos: usize) -> bool {
+    (n & (1 << (NUM_LEN - 1 - pos))) != 0
+}
+
 pub fn part1(input: &Input) -> u32 {
-    let mut freq = vec![(0, 0); input[0].len()];
-    for w in input {
-        for (i, &c) in w.iter().enumerate() {
-            if c {
-                freq[i].1 += 1;
-            } else {
-                freq[i].0 += 1;
-            }
+    let mut freqs = vec![[0; 2]; NUM_LEN];
+    for &w in input {
+        for (i, freq) in freqs.iter_mut().enumerate() {
+            freq[get_bit(w, i) as usize] += 1;
         }
     }
-    let gamma = freq.iter().fold(0, |acc, (z, u)| {
-        (acc << 1) + (u > z) as u32
-    });
-    let eps = freq
+    let gamma = freqs
         .iter()
-        .fold(0, |acc, (z, u)| (acc << 1) + (u < z) as u32);
+        .fold(0, |acc, [z, u]| (acc << 1) + (u > z) as u32);
+    let eps = (1 << NUM_LEN) - 1 - gamma;
     gamma * eps
 }
 
 pub fn part2(input: &Input) -> u32 {
-    let mut nums = input.clone();
-    let mut pos = 0;
-    while nums.len() > 1 {
-        let mut freq = (0, 0);
-        for c in &nums {
-            if c[pos] {
-                freq.1 += 1;
-            } else {
-                freq.0 += 1;
+    fn find_rating(input: &Input, select: impl Fn(u16, u16) -> bool) -> u32 {
+        let mut nums = input.clone();
+        let mut pos = 0;
+        while nums.len() > 1 {
+            let mut freq = [0; 2];
+            for &c in &nums {
+                freq[get_bit(c, pos) as usize] += 1;
             }
+            nums.retain(|&n| get_bit(n, pos) == select(freq[0], freq[1]));
+            pos += 1;
         }
-
-        nums.retain(|n| n[pos] == (freq.1 >= freq.0));
-        pos += 1;
+        nums[0] as u32
     }
-    let oxy = nums[0].iter().fold(0, |acc, d| (acc << 1) + *d as u32);
 
-    let mut nums = input.clone();
-    let mut pos = 0;
-    while nums.len() > 1 {
-        let mut freq = (0, 0);
-        for c in &nums {
-            if c[pos] {
-                freq.1 += 1;
-            } else {
-                freq.0 += 1;
-            }
-        }
-
-        nums.retain(|n| n[pos] == (freq.1 < freq.0));
-        pos += 1;
-    }
-    let co2 = nums[0].iter().fold(0, |acc, d| (acc << 1) + *d as u32);
+    let oxy = find_rating(input, |z, u| u >= z);
+    let co2 = find_rating(input, |z, u| u < z);
 
     oxy * co2
 }
