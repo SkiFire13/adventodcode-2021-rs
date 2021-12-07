752c60c2eb71c4b385a40c9f7c2d6c2ab52349dc
Giacomo Stevanato <giaco.stevanato@gmail.com>
2021-12-07 17:19:34 +0100

    Improve day7 performance and time complexity

--- src/day7.rs
+++ src/day7.rs
@@ -6,29 +6,27 @@ pub fn input_generator(input: &str) -> Input {
     input
         .split(',')
         .map(|l| l.parse().expect("Invalid input"))
-        .sorted()
         .collect()
 }
 
-fn best_fuel_cost(positions: &[u32], fuel_cost: impl Fn(u32) -> u32) -> u32 {
+fn calculate_cost(positions: &[u32], dest: u32, fuel_cost: fn(u32) -> u32) -> u32 {
     positions
         .iter()
-        .map(|&d| {
-            positions
-                .iter()
-                .map(|&p| fuel_cost(u32::max(d, p) - u32::min(d, p)))
-                .sum::<u32>()
-        })
-        .tuple_windows()
-        .min_by_key(|&(c1, c2)| c2 > c1)
-        .map(|(c1, _)| c1)
-        .unwrap()
+        .map(|&pos| u32::max(dest, pos) - u32::min(dest, pos))
+        .map(fuel_cost)
+        .sum()
 }
 
-pub fn part1(input: &Input) -> u32 {
-    best_fuel_cost(input, |dist| dist)
+pub fn part1(input: &mut Input) -> u32 {
+    let len = input.len();
+    let (_, &mut median, _) = input.select_nth_unstable(len / 2);
+    calculate_cost(input, median, |dist| dist)
 }
 
 pub fn part2(input: &Input) -> u32 {
-    best_fuel_cost(input, |dist| dist * (dist + 1) / 2)
+    let mean = input.iter().copied().sum::<u32>() / input.len() as u32;
+    (mean..=mean+2)
+        .map(|dest| calculate_cost(input, dest, |dest| dest * (dest + 1) / 2))
+        .min()
+        .unwrap()
 }
