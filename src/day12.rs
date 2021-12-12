d162b709791f7970686f2131762c9ad9d5b2bb54
Giacomo Stevanato <giaco.stevanato@gmail.com>
2021-12-12 10:31:59 +0100

    Use a bitset for day 12 solution

--- src/day12.rs
+++ src/day12.rs
@@ -1,44 +1,49 @@
 #[allow(unused_imports)]
 use super::prelude::*;
-type Input<'a> = HashMap<&'a str, Vec<&'a str>>;
+type Input<'a> = Vec<u16>;
 
 pub fn input_generator(input: &str) -> Input {
-    let mut map = HashMap::new();
+    let mut map = HashMap::from([("start", 0), ("end", 1)]);
+    let mut edges = vec![1 << 15; 2];
     for line in input.lines() {
-        let (first, second) = line.split_once('-').unwrap();
-        map.entry(first).or_insert(Vec::new()).push(second);
-        map.entry(second).or_insert(Vec::new()).push(first);
+        let (n1, n2) = line.split_once('-').unwrap();
+        let mut idx_entry = |n| *map.entry(n).or_insert_with(|| {
+            edges.push((n.as_bytes()[0].is_ascii_lowercase() as u16) << 15);
+            edges.len() - 1
+        });
+        let idx1 = idx_entry(n1);
+        let idx2 = idx_entry(n2);
+        edges[idx1] |= 1 << idx2;
+        edges[idx2] |= 1 << idx1;
     }
-    map
+    edges
 }
 
 pub fn paths_num<'a>(
-    start: &'a str,
+    start: usize,
     input: &Input<'a>,
-    visited: &mut HashSet<&'a str>,
-    allow_twice: &mut bool,
+    visited: u16,
+    allow_twice: bool,
 ) -> usize {
-    if start == "end" {
+    if start == 1 {
         return 1;
     }
     let mut num = 0;
-    for &dest in &input[start] {
-        if visited.insert(dest) || dest.as_bytes()[0].is_ascii_uppercase() {
-            num += paths_num(dest, input, visited, allow_twice);
-            visited.remove(dest);
-        } else if *allow_twice == true && dest != "start" {
-            *allow_twice = false;
-            num += paths_num(dest, input, visited, allow_twice);
-            *allow_twice = true;
+    let neighbours = input[start];
+    for dest in (0..15).filter(|&idx| neighbours & (1 << idx) != 0) {
+        if visited & (1 << dest) == 0 || input[dest] & (1 << 15) == 0 {
+            num += paths_num(dest, input, visited | (1 << dest), allow_twice);
+        } else if allow_twice == true && dest != 0 {
+            num += paths_num(dest, input, visited, false);
         }
     }
     num
 }
 
 pub fn part1(input: &Input) -> usize {
-    paths_num("start", input, &mut HashSet::from(["start"]), &mut false)
+    paths_num(0, input, 1, false)
 }
 
 pub fn part2(input: &Input) -> usize {
-    paths_num("start", input, &mut HashSet::from(["start"]), &mut true)
+    paths_num(0, input, 1, true)
 }
