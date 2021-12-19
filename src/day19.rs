2f5871626683e2d42336c8f3999ebf5ad295adbb
Giacomo Stevanato <giaco.stevanato@gmail.com>
2021-12-19 10:24:39 +0100

    Improve day 19 performance

--- src/day19.rs
+++ src/day19.rs
@@ -1,6 +1,6 @@
 #[allow(unused_imports)]
 use super::prelude::*;
-type Input = Vec<HashSet<[i32; 3]>>;
+type Input = Vec<HashSet<[i16; 3]>>;
 
 pub fn input_generator(input: &str) -> Input {
     input
@@ -19,8 +19,8 @@ pub fn input_generator(input: &str) -> Input {
         .collect()
 }
 
-type Rot = [[i32; 3]; 3];
-type Vet = [i32; 3];
+type Rot = [[i16; 3]; 3];
+type Vet = [i16; 3];
 
 // m2 is applied first
 const fn mat_x_mat(m1: Rot, m2: Rot) -> Rot {
@@ -70,29 +70,55 @@ const ROT_LEFT: Rot = mat_x_mat(ROT_BEHIND, ROT_RIGHT);
 const ROTS_X: [Rot; 4] = [ROT_NOP, ROT_X, ROT_XX, ROT_XXX];
 const ROTS_AXIS: [Rot; 6] = [ROT_NOP, ROT_UP, ROT_DOWN, ROT_BEHIND, ROT_LEFT, ROT_RIGHT];
 
-fn resolve_positions(input: &Input) -> FxHashMap<usize, ([i32; 3], HashSet<[i32; 3]>)> {
-    let mut found: FxHashMap<usize, ([i32; 3], HashSet<[i32; 3]>)> =
-        FxHashMap::from_iter([(0, ([0; 3], input[0].clone()))]);
-    let mut visited: FxHashSet<(usize, usize)> = FxHashSet::default();
+const ROTS: [Rot; 24] = {
+    let mut rots = [ROT_NOP; 24];
+    let mut i = 0;
+    while i < 6 {
+        let mut j = 0;
+        while j < 4 {
+            rots[i * 4 + j] = mat_x_mat(ROTS_X[j], ROTS_AXIS[i]);
+            j += 1;
+        }
+        i += 1;
+    }
+    rots
+};
 
-    while found.len() < input.len() {
+fn resolve_positions(input: &Input) -> Vec<([i16; 3], HashSet<[i16; 3]>)> {
+    let mut num_found = 1;
+    let mut found = vec![None; input.len()];
+    found[0] = Some(([0; 3], input[0].clone()));
+
+    let mut visited = Grid {
+        vec: vec![false; input.len() * input.len()],
+        width: input.len(),
+    };
+
+    let rotated_points = input
+        .iter()
+        .map(|points| {
+            ROTS.map(|rot| {
+                points
+                    .iter()
+                    .map(|&p| mat_x_vet(rot, p))
+                    .collect::<Vec<_>>()
+            })
+        })
+        .collect::<Vec<_>>();
+
+    while num_found < input.len() {
         'i: for i in 0..input.len() {
-            if found.contains_key(&i) {
+            if found[i].is_some() {
                 continue 'i;
             }
-            'j: for j in found.keys().copied() {
-                if !visited.insert((i, j)) {
+            'j: for j in 0..input.len() {
+                if found[j].is_none() || replace(&mut visited[(i, j)], true) {
                     continue 'j;
                 }
-
-                for (rot_x, rot_axis) in itertools::iproduct!(ROTS_X, ROTS_AXIS) {
-                    let rot = mat_x_mat(rot_x, rot_axis);
-                    let points = input[i]
-                        .iter()
-                        .map(|&p| mat_x_vet(rot, p))
-                        .collect::<Vec<_>>();
-                    for point in &points {
-                        for &candidate in &found[&j].1 {
+                for points in &rotated_points[i] {
+                    for point in &points[11..] {
+                        let candidates = &found[j].as_ref().unwrap().1;
+                        for &candidate in candidates.iter().take(candidates.len() - 11) {
                             let root = [
                                 candidate[0] - point[0],
                                 candidate[1] - point[1],
@@ -101,14 +127,15 @@ fn resolve_positions(input: &Input) -> FxHashMap<usize, ([i32; 3], HashSet<[i32;
                             let num_intersections = points
                                 .iter()
                                 .map(|&p| [p[0] + root[0], p[1] + root[1], p[2] + root[2]])
-                                .filter(|p| found[&j].1.contains(p))
+                                .filter(|p| candidates.contains(p))
                                 .count();
                             if num_intersections >= 12 {
                                 let points = points
                                     .iter()
                                     .map(|&p| [p[0] + root[0], p[1] + root[1], p[2] + root[2]])
                                     .collect();
-                                found.insert(i, (root, points));
+                                found[i] = Some((root, points));
+                                num_found += 1;
                                 continue 'i;
                             }
                         }
@@ -118,22 +145,22 @@ fn resolve_positions(input: &Input) -> FxHashMap<usize, ([i32; 3], HashSet<[i32;
         }
     }
 
-    found
+    found.into_iter().map(Option::unwrap).collect()
 }
 
 pub fn part1(input: &Input) -> usize {
     resolve_positions(input)
-        .values()
-        .flat_map(|(_, beacons)| beacons.iter().copied())
+        .into_iter()
+        .flat_map(|(_, beacons)| beacons.into_iter())
         .unique()
         .count()
 }
 
-pub fn part2(input: &Input) -> i32 {
+pub fn part2(input: &Input) -> i16 {
     let found = resolve_positions(input);
     (0..input.len())
         .flat_map(|i| (i + 1..input.len()).map(move |j| (i, j)))
-        .map(|(i, j)| (found[&i].0, found[&j].0))
+        .map(|(i, j)| (found[i].0, found[j].0))
         .map(|(p1, p2)| (p1[0] - p2[0]).abs() + (p1[1] - p2[1]).abs() + (p1[2] - p2[2]).abs())
         .max()
         .unwrap()
