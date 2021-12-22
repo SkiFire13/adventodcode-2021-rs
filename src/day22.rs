4442a8df8a38a1c3cd33d6e08b7980035b5e2013
Giacomo Stevanato <giaco.stevanato@gmail.com>
2021-12-22 15:29:51 +0100

    Improve day 22 solution performance

--- src/day22.rs
+++ src/day22.rs
@@ -13,9 +13,9 @@ pub fn input_generator(input: &str) -> Input {
             let (y0, y1) = y.split_once("..").unwrap();
             let (z0, z1) = z.split_once("..").unwrap();
             let coords = [
-                [x0.parse().unwrap(), x1.parse().unwrap()],
-                [y0.parse().unwrap(), y1.parse().unwrap()],
-                [z0.parse().unwrap(), z1.parse().unwrap()],
+                [x0.parse().unwrap(), x1.parse::<i32>().unwrap() + 1],
+                [y0.parse().unwrap(), y1.parse::<i32>().unwrap() + 1],
+                [z0.parse().unwrap(), z1.parse::<i32>().unwrap() + 1],
             ];
             (onoff == "on", coords)
         })
@@ -29,9 +29,15 @@ fn find_on_area(input: impl Iterator<Item = (bool, [[i32; 2]; 3])>) -> usize {
         'cubes: while i < cubes.len() {
             let cubei = cubes[i];
 
-            if cubei == cube {
-                cubes.swap_remove(i);
-                break 'cubes;
+            if cube[0][0] >= cubei[0][1]
+                || cube[0][1] <= cubei[0][0]
+                || cube[1][0] >= cubei[1][1]
+                || cube[1][1] <= cubei[1][0]
+                || cube[2][0] >= cubei[2][1]
+                || cube[2][1] <= cubei[2][0]
+            {
+                i += 1;
+                continue 'cubes;
             }
 
             let clip = [
@@ -40,30 +46,23 @@ fn find_on_area(input: impl Iterator<Item = (bool, [[i32; 2]; 3])>) -> usize {
                 [max(cube[2][0], cubei[2][0]), min(cube[2][1], cubei[2][1])],
             ];
 
-            for axis in clip {
-                if axis[0] > axis[1] {
-                    i += 1;
-                    continue 'cubes;
-                }
-            }
-
             if cubei[0][0] != clip[0][0] {
-                cubes.push([[cubei[0][0], clip[0][0] - 1], cubei[1], cubei[2]]);
+                cubes.push([[cubei[0][0], clip[0][0]], cubei[1], cubei[2]]);
             }
             if cubei[0][1] != clip[0][1] {
-                cubes.push([[clip[0][1] + 1, cubei[0][1]], cubei[1], cubei[2]]);
+                cubes.push([[clip[0][1], cubei[0][1]], cubei[1], cubei[2]]);
             }
             if cubei[1][0] != clip[1][0] {
-                cubes.push([clip[0], [cubei[1][0], clip[1][0] - 1], cubei[2]]);
+                cubes.push([clip[0], [cubei[1][0], clip[1][0]], cubei[2]]);
             }
             if cubei[1][1] != clip[1][1] {
-                cubes.push([clip[0], [clip[1][1] + 1, cubei[1][1]], cubei[2]]);
+                cubes.push([clip[0], [clip[1][1], cubei[1][1]], cubei[2]]);
             }
             if cubei[2][0] != clip[2][0] {
-                cubes.push([clip[0], clip[1], [cubei[2][0], clip[2][0] - 1]]);
+                cubes.push([clip[0], clip[1], [cubei[2][0], clip[2][0]]]);
             }
             if cubei[2][1] != clip[2][1] {
-                cubes.push([clip[0], clip[1], [clip[2][1] + 1, cubei[2][1]]]);
+                cubes.push([clip[0], clip[1], [clip[2][1], cubei[2][1]]]);
             }
 
             cubes.swap_remove(i);
@@ -76,9 +75,9 @@ fn find_on_area(input: impl Iterator<Item = (bool, [[i32; 2]; 3])>) -> usize {
     cubes
         .iter()
         .map(|&cube| {
-            ((cube[0][1] - cube[0][0] + 1) as usize)
-                * ((cube[1][1] - cube[1][0] + 1) as usize)
-                * ((cube[2][1] - cube[2][0] + 1) as usize)
+            ((cube[0][1] - cube[0][0]) as usize)
+                * ((cube[1][1] - cube[1][0]) as usize)
+                * ((cube[2][1] - cube[2][0]) as usize)
         })
         .sum()
 }
@@ -86,17 +85,17 @@ fn find_on_area(input: impl Iterator<Item = (bool, [[i32; 2]; 3])>) -> usize {
 pub fn part1(input: &Input) -> usize {
     let clip = |cube: [[i32; 2]; 3]| {
         [
-            [max(cube[0][0], -50), min(cube[0][1], 50)],
-            [max(cube[1][0], -50), min(cube[1][1], 50)],
-            [max(cube[2][0], -50), min(cube[2][1], 50)],
+            [max(cube[0][0], -50), min(cube[0][1], 50 + 1)],
+            [max(cube[1][0], -50), min(cube[1][1], 50 + 1)],
+            [max(cube[2][0], -50), min(cube[2][1], 50 + 1)],
         ]
     };
     let clipped_input = input
         .iter()
         .map(|&(onoff, cube)| (onoff, clip(cube)))
-        .filter(|(_, cube)| cube[0][0] <= cube[0][1])
-        .filter(|(_, cube)| cube[1][0] <= cube[1][1])
-        .filter(|(_, cube)| cube[2][0] <= cube[2][1]);
+        .filter(|(_, cube)| cube[0][0] < cube[0][1])
+        .filter(|(_, cube)| cube[1][0] < cube[1][1])
+        .filter(|(_, cube)| cube[2][0] < cube[2][1]);
 
     find_on_area(clipped_input)
 }
