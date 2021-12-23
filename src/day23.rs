6aa366542d86363adf337f15e02bccc289d09497
Giacomo Stevanato <giaco.stevanato@gmail.com>
2021-12-23 13:46:51 +0100

    Refactor day 23 solution

--- src/day23.rs
+++ src/day23.rs
@@ -1,6 +1,6 @@
 #[allow(unused_imports)]
 use super::prelude::*;
-type Input = Vec<(u8, u8, u8)>;
+type Input = State<2>;
 
 pub fn input_generator(input: &str) -> Input {
     let mut out = vec![];
@@ -11,107 +11,101 @@ pub fn input_generator(input: &str) -> Input {
             }
         }
     }
-    out.sort_unstable();
-    out
+    State::from_vec(out)
 }
 
-fn smallest_cost<const DEPTH: usize>(input: Input) -> u32 {
+#[derive(Clone, Hash, PartialEq, Eq)]
+pub struct State<const DEPTH: usize> {
+    inner: Box<[(u8, u8, u8)]>
+}
+
+impl<const DEPTH: usize> State<DEPTH> {
+    fn from_vec(mut v: Vec<(u8, u8, u8)>) -> Self {
+        v.sort_unstable();
+        Self { inner: v.into_boxed_slice() }
+    }
+    fn to_vec(&self) -> Vec<(u8, u8, u8)> {
+        self.inner.to_vec()
+    }
+    fn with(&self, idx: usize, x: u8, y: u8) -> Self {
+        let mut state = self.clone();
+        state.inner[idx] = (self.inner[idx].0, x, y);
+        state.inner.sort_unstable();
+        state
+    }
+    fn enumerate(&self) -> impl Iterator<Item = (usize, u8, u8, u8)> + '_ {
+        self.inner.iter().enumerate().map(|(idx, &(c, x, y))| (idx, c, x, y))
+    }
+    fn any(&self, mut f: impl FnMut(u8, u8, u8) -> bool) -> bool {
+        self.inner.iter().any(|&(c, x, y)| f(c, x, y))
+    }
+    fn count(&self, mut f: impl FnMut(u8, u8, u8) -> bool) -> u8 {
+        self.inner.iter().filter(|&&(c, x, y)| f(c, x, y)).count() as u8
+    }
+}
+
+fn smallest_cost<const DEPTH: usize>(input: State<DEPTH>) -> u32 {
     let depth = DEPTH as u8;
+
+    #[ord_by_key(|this| Reverse(this.cost))]
+    struct Entry<const DEPTH: usize> {
+        cost: u32,
+        state: State<DEPTH>,
+    }
 
     let mut queue = BinaryHeap::new();
     let mut seen = HashSet::new();
-    queue.push((Reverse(0), input.into_boxed_slice()));
-    while let Some((Reverse(cost), state)) = queue.pop() {
-        if seen.insert(state.clone()) {
-            // Final check
-            if state.iter().all(|&(c, x, _)| x == 2*(c+1)) {
-                return cost;
-            }
+    queue.push(Entry { cost: 0, state: input });
+    while let Some(Entry { cost, state }) = queue.pop() {
+        if !seen.insert(state.clone()) {
+            continue;
+        }
 
-            // Possible moves for each character
-            'c: for (idx, &(c, x, y)) in state.iter().enumerate() {
-                // Check if already in final position
-                if x == 2*(c+1) {
-                    if state.iter().filter(|&&(fc, fx, fy)| fc == c && fx == x && fy > y).count() as u8 == depth - y {
-                        continue 'c;
-                    }
-                }
+        if !state.any(|c, x, _| x != 2*(c+1)) {
+            return cost;
+        }
 
-                // Check if moveable up
-                if y > 0 && state.iter().any(|&(_, ax, ay)| ax == x && ay < y) {
+        'c: for (idx, c, x, y) in state.enumerate() {
+            if x == 2*(c+1) {
+                if state.count(|fc, fx, fy| fc == c && fx == x && fy > y) == depth - y {
                     continue 'c;
                 }
-
-                // Going to left
-                'xtarg1: for xtarg in (0..x).rev() {
-                    // Move to destination
-                    if xtarg == 2*(c+1) {
-                        // No amphipod from other rooms
-                        if !state.iter().any(|&(ac, ax, _)| ac != c && ax == xtarg) {
-                            let count = state.iter().filter(|&&(_, fx, _)| fx == xtarg).count() as u8;
-                            let mut state = state.clone();
-                            state[idx] = (c, xtarg, depth - count);
-                            state.sort_unstable();
-                            queue.push((Reverse(cost + (y + x - xtarg + depth - count) as u32 * 10u32.pow(c as u32)), state));
-                        }
-                        // Can only move to destination if in the hallway
-                        if y == 0 {
-                            break 'xtarg1;
-                        }
-                    }
-                    // Can't move outside doors
-                    if matches!(xtarg, 2 | 4 | 6 | 8) {
-                        continue 'xtarg1;
-                    }
-                    // Can't jump characters
-                    if state.iter().any(|&(_, ax, _)| ax == xtarg) {
-                        break 'xtarg1;
-                    }
-                    if y == 0 {
-                        continue 'xtarg1;
-                    }
-                    // Move
-                    let mut state = state.clone();
-                    state[idx] = (c, xtarg, 0);
-                    state.sort_unstable();
-                    queue.push((Reverse(cost + (y + x - xtarg) as u32 * 10u32.pow(c as u32)), state));
-                }
-
-                // Going to right
-                'xtarg2: for xtarg in x+1..=10 {
-                    // Move to destination
-                    if xtarg == 2*(c+1) {
-                        // No amphipod from other rooms
-                        if !state.iter().any(|&(ac, ax, _)| ac != c && ax == xtarg) {
-                            let count = state.iter().filter(|&&(_, fx, _)| fx == xtarg).count() as u8;
-                            let mut state = state.clone();
-                            state[idx] = (c, xtarg, depth - count);
-                            state.sort_unstable();
-                            queue.push((Reverse(cost + (y + xtarg - x + depth - count) as u32 * 10u32.pow(c as u32)), state));
-                        }
-                        // Can only move to destination if in the hallway
-                        if y == 0 {
-                            break 'xtarg2;
-                        }
-                    }
-                    // Can't move outside doors
-                    if matches!(xtarg, 2 | 4 | 6 | 8) {
-                        continue 'xtarg2;
-                    }
-                    // Can't jump characters
-                    if state.iter().any(|&(_, ax, _)| ax == xtarg) {
-                        break 'xtarg2;
-                    }
-                    if y == 0 {
-                        continue 'xtarg2;
-                    }
-                    // Move
-                    let mut state = state.clone();
-                    state[idx] = (c, xtarg, 0);
-                    state.sort_unstable();
-                    queue.push((Reverse(cost + (y + xtarg - x) as u32 * 10u32.pow(c as u32)), state));
-                }
             }
+
+            if y > 0 && state.any(|_, ax, ay| ax == x && ay < y) {
+                continue 'c;
+            }
+
+            use std::ops::ControlFlow;
+            let mut move_to_x = |xtarg| {
+                let dx = if x < xtarg { xtarg - x } else { x - xtarg };
+                if xtarg == 2*(c+1) {
+                    if !state.any(|ac, ax, _| ac != c && ax == xtarg) {
+                        let count = state.count(|_, fx, _| fx == xtarg);
+                        let cost = cost + (y + dx + depth - count) as u32 * 10u32.pow(c as u32);
+                        let state = state.with(idx, xtarg, depth - count);
+                        queue.push(Entry { cost, state });
+                    }
+                    if y == 0 {
+                        return ControlFlow::Break(());
+                    }
+                }
+                if matches!(xtarg, 2 | 4 | 6 | 8) {
+                    return ControlFlow::Continue(());
+                }
+                if state.any(|_, ax, _| ax == xtarg) {
+                    return ControlFlow::Break(());
+                }
+                if y != 0 {
+                    let cost = cost + (y + dx) as u32 * 10u32.pow(c as u32);
+                    let state = state.with(idx, xtarg, 0);
+                    queue.push(Entry { cost, state });
+                }
+                ControlFlow::Continue(())
+            };
+
+            (0..x).rev().try_for_each(&mut move_to_x);
+            (x+1..11).try_for_each(&mut move_to_x);
         }
     }
     panic!();
@@ -122,15 +116,12 @@ pub fn part1(input: &Input) -> u32 {
 }
 
 pub fn part2(input: &Input) -> u32 {
-    let mut input = input.clone();
+    let mut input = input.to_vec();
     input.iter_mut().filter(|(_, _, y)| *y == 2).for_each(|(_, _, y)| *y += 2);
     for (y, l) in [['D', 'C', 'B', 'A'], ['D', 'B', 'A', 'C']].into_iter().enumerate() {
         for (x, c) in l.into_iter().enumerate() {
             input.push((c as u8 - b'A', 2 * (x + 1) as u8, 2 + y as u8))
         }
     }
-    input.sort_unstable();
-
-    
-    smallest_cost::<4>(input)
+    smallest_cost::<4>(State::from_vec(input))
 }
